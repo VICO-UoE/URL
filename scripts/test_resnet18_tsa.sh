340c969d84e84b29554cfc196132d6f38613f43e
WeiHongLee <whleesysu@gmail.com>
2022-06-28 15:55:14 +0100

    update for tsa in sdl

--- scripts/test_resnet18_tsa.sh
+++ scripts/test_resnet18_tsa.sh
@@ -1,4 +1,4 @@
 ################### Test URL Model with Task-specific Adapters (TSA) ###################
 # url + residual adapters (matrix, initialized as identity matrices) + pre-classifier alignment 
 CUDA_VISIBLE_DEVICES=<gpu-id> python test_extractor_tsa.py --model.name=url --model.dir ./saved_results/url \
--test.tsa-ad-type residual --test.tsa-ad-form matrix --test.tsa-opt alpha+beta --test.tsa-init eye
+-test.tsa-ad-type residual --test.tsa-ad-form matrix --test.tsa-opt alpha+beta --test.tsa-init eye --test.mode mdl
