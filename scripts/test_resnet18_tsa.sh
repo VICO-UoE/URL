997265f7f9471c240a9cc4e053758fe552acd0db
WeiHongLee <whleesysu@gmail.com>
2022-11-21 21:58:40 +0000

    update

--- scripts/test_resnet18_tsa.sh
+++ scripts/test_resnet18_tsa.sh
@@ -9,4 +9,4 @@
 # an example for the best configuration
 # url + residual adapters (matrix, initialized as identity matrices) + pre-classifier alignment 
 CUDA_VISIBLE_DEVICES=<gpu-id> python test_extractor_tsa.py --model.name=url --model.dir ./saved_results/url \
--test.tsa-ad-type residual --test.tsa-ad-form matrix --test.tsa-opt alpha+beta --test.tsa-init eye --test.mode mdl
+--test.tsa-ad-type residual --test.tsa-ad-form matrix --test.tsa-opt alpha+beta --test.tsa-init eye --test.mode mdl
